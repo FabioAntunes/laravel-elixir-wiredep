323098018dff5e59831a96ed48a530d44175e1cc
Fábio Antunes <fabioantuness@gmail.com>
2015-12-20 19:57:12 +0000

    Added support to wiredep Less, Sass and Scss

--- index.js
+++ index.js
@@ -2,31 +2,30 @@ var gulp = require('gulp');
 var wiredep = require('wiredep').stream;
 var elixir = require('laravel-elixir');
 var path = require('path');
+var wiredepConfig = require('./config');
 var task = elixir.Task;
 
-elixir.extend('wiredep', function(config, opts) {
+elixir.extend('wiredep', function(type, config, opts) {
 
-  var config = config || {};
-  var opts = opts || {};
+  //for legacy reasons
+  if(type === Object(type)){
+    var config = type || {};
+    var opts = config || {};
+    var type = 'php';
+    var typeDefaults = wiredepConfig[type];
+  }else{
+    var type = wiredepConfig.hasOwnProperty(type) ? type : 'php';
+    var typeDefaults = wiredepConfig[type];
+    var config = config || {};
+    var opts = opts || {};
+  }
 
-  config.baseDir = config.baseDir || 'resources/views/';
-  config.src = config.src || false;
-  config.searchLevel = config.searchLevel || '**/*.php';
+  config.baseDir = config.baseDir || typeDefaults.config.baseDir;
+  config.src = config.src || typeDefaults.config.src;
+  config.searchLevel = config.searchLevel || typeDefaults.config.searchLevel;
 
-  opts.ignorePath = opts.ignorePath || /(\..\/)*public/;
-  opts.fileTypes = opts.fileTypes || {
-        php: {
-          block: /(([ \t]*)<!--\s*bower:*(\S*)\s*-->)(\n|\r|.)*?(<!--\s*endbower\s*-->)/gi,
-          detect: {
-            js: /<script.*src=['"]([^'"]+)/gi,
-            css: /<link.*href=['"]([^'"]+)/gi
-          },
-          replace: {
-            js: '<script src="{{filePath}}"></script>',
-            css: '<link rel="stylesheet" href="{{filePath}}" />'
-          }
-        }
-      };
+  opts.ignorePath = opts.ignorePath || typeDefaults.opts.ignorePath;
+  opts.fileTypes = opts.fileTypes || typeDefaults.opts.fileTypes;
 
       new task('wiredep', function() {
         var src = path.join(config.baseDir, !!config.src ? config.src : config.searchLevel);
